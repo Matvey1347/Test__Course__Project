b5fcaf4cf7578b447463b788e3264a5ea30d6ceb
Matvey <106485919+Matvey1347@users.noreply.github.com>
2022-12-02 00:11:08 +0200

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @ViewChild('optionsWrap') optionsWrapRef!: ElementRef;
+  @ViewChild('select') selectRef!: ElementRef;
+
+  openedSelect = false;
+  language = 'EN';
+
+  constructor() { }
+
+  @HostListener('document:click', ['$event.target'])
+  onCloseClick(target: HTMLElement): void {
+    if (this.optionsWrapRef) {
+      const clickedInside = this.optionsWrapRef.nativeElement.contains(target);
+      const clickOnSelect = this.selectRef.nativeElement.contains(target);
+      if (!clickedInside && !clickOnSelect && this.openedSelect) {
+        this.openedSelect = false
+      }
+    }
+  }
+}
